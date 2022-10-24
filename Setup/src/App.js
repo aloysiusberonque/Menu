a7f7632f1a3bb898ffab80f9b1a72dd960de35d3
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-10-24 20:02:52 +0800

    Added page title

--- Setup/src/App.js
+++ Setup/src/App.js
@@ -4,7 +4,16 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return(
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
